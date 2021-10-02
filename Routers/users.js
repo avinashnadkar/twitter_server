27d9dc77e812961654d33d2f7ac82b5dd95fe85b
avinashnadkar <avinashnadkar3@gmail.com>
2021-10-02 15:29:27 +0530

    updated req.body parameters

--- Routers/users.js
+++ Routers/users.js
@@ -8,19 +8,11 @@ router.get('/',(req,res)=>{
 //Register user
 router.post('/',(req,res)=>{
        const newUser = new User({
-        // profilePic :  null,
-        // coverPhoto : null,
         name : req.body.name,
-        // username : req.body.username,
         password : req.body.password,
         phone : req.body.phone,
         email : req.body.email,
         birthDate : req.body.birthDate
-        // about : req.body.about,
-        // userInfo : req.body.user_info,
-        // tweets : req.body.tweets
-        // following :  req.body.tweets,
-        // followers :  req.body.tweets
    })
 
     newUser.save()
