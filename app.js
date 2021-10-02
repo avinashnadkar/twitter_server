ad74b67d07afc600402d61ca054b342431c67c58
avinashnadkar <avinashnadkar3@gmail.com>
2021-10-02 15:22:46 +0530

    Connected to mongoDB using nodemon, protected database admin credentials in .env file

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+require('dotenv').config()
+const mongoose = require("mongoose")
+const cors = require("cors")
 const app = express();
+const port = process.env.PORT || 2345;
+const usersRouter = require('./Routers/users')
 
-app.get('/',(res,req)=>{
-    req.send('server is running')
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//Connect to database. the username and password is safe in .env file
+mongoose.connect(process.env.DB_URI,{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
 })
 
-app.listen(2345, (err)=>{
+//TO check if databse connected
+const db = mongoose.connection;
+db.once('open', () => console.log('connected to db'))
+
+
+//Route req to users Router
+app.use('/users',usersRouter)
+
+app.listen(port, (err)=>{
    if(err){
        console.log(err)
    }else{
