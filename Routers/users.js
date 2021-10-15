2d8c6be3555479233fd5880f101584cf90a0f8fc
avinashnadkar <avinashnadkar3@gmail.com>
2021-10-15 19:42:15 +0530

    Changed response after login user added name,username and avtar

--- Routers/users.js
+++ Routers/users.js
@@ -49,21 +49,21 @@ router.post('/signup', async (req, res) => {
                 status: "success",
                 code: 200,
                 message: "Welcome to Twitter.",
-                results :{
+                results: {
                     token: token,
-                    name : name
+                    name: name
                 }
-            })  
+            })
         }).catch((err) => {
             res.status(400).json("error :" + err)
         })
 })
 
 //Login user
-router.post('/login', async (req,res) => {
-    const {email,password} = req.body;
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
-    let user = await User.findOne({email: email});
+    let user = await User.findOne({ email: email });
     if (!user) return res.status(400).send('Invalid Email or Password.')
 
     // let user = User.find(user=>{
@@ -76,11 +76,11 @@ router.post('/login', async (req,res) => {
     //     })
     // }
 
-    let isMatch = await bcrypt.compare(password,user.password)
-    
-    if(!isMatch){
+    let isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
         return res.status(400).json({
-            "errors" : [{"msg" : "invalid credentials"}]
+            "errors": [{ "msg": "invalid credentials" }]
         })
     }
 
@@ -91,56 +91,59 @@ router.post('/login', async (req,res) => {
     }, process.env.KEY, {
         expiresIn: 72000000
     })
- 
+
     res.json({
         status: "success",
         code: 200,
         message: "Welcome to Twitter.",
-        results :{
+        results: {
             token: token,
-            u_id : user._id,
+            u_id: user._id,
+            name: user.name,
+            username: user.username,
+            avtar : user.profilePic,
         }
     })
 })
 
 
 //Get all users
-router.get('/all', checkAuth ,(req,res)=>{
+router.get('/all', checkAuth, (req, res) => {
     User.find()
-    .then(users => {
-        res.json(users)
-    }).catch(err => {
-        res.status(400).json('Error :' + err)
-    })
+        .then(users => {
+            res.json(users)
+        }).catch(err => {
+            res.status(400).json('Error :' + err)
+        })
 })
 
 //Get user info
-router.get('/:id',checkAuth,(req,res)=>{
+router.get('/:id', checkAuth, (req, res) => {
     let id = req.params.id;
     User.findById(id)
-    .then(user=>{
-        if(user != null){
-            res.json({
-                avtar : user.profilePic,
-                coverPhoto : user.coverPhoto,
-                name : user.name,
-                username : user.username,
-                about  : user.about,
-                phone : user.phone,
-                email : user.email,
-                following : user.following,
-                followers : user.followers,
-                status : user.status,
-                birthDate : user.birthDate
-            })
-        }else{
-            res.status(400).json({
-                "errors" : [{"msg" : "invalid user id"}]
-            })
-        }
-    }).catch(err=>{
-        res.status(400).json('Error:' + err)
-    })
+        .then(user => {
+            if (user != null) {
+                res.json({
+                    avtar: user.profilePic,
+                    coverPhoto: user.coverPhoto,
+                    name: user.name,
+                    username: user.username,
+                    about: user.about,
+                    phone: user.phone,
+                    email: user.email,
+                    following: user.following,
+                    followers: user.followers,
+                    status: user.status,
+                    birthDate: user.birthDate
+                })
+            } else {
+                res.status(400).json({
+                    "errors": [{ "msg": "invalid user id" }]
+                })
+            }
+        }).catch(err => {
+            res.status(400).json('Error:' + err)
+        })
 })
 
 
